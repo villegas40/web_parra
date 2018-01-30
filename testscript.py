31cdcfb8c364a7f46efacc47ab75ce236d6244fc
brenda <barbozabrenda3@gmail.com>
2018-01-30 15:17:59 -0800

    agregar funcion

--- testscript.py
+++ testscript.py
@@ -1,5 +1,9 @@
 import math
 
+def seno(num):
+	res= math.sin(num)
+	return res
+
 def coseno(num):
     res = math.cos(num)
     return res
@@ -10,3 +14,4 @@ base = int(input("Ingrese el valor de la base: "))
 
 print("La base mide %s" %base)
 print(coseno(num))
+
